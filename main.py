b66bab94ebd61f7673442b32b05573df0c3d5e96
DanielsZubrinov <DZ0412@s1vsk.lv>
2021-01-07 08:52:59 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+ #Komentārus var rakstīt
+ #vairākās
+#rindās"""
+print("Sveika pasaule!")
+#print(2*2)
+print(2*2,2*3,2*4,"Pitons")
+print(f"Ja saskaitīsim 5 ar 7, iegūsim {5+7}. ")
+print("Sveika, "+"pasaulei!") #var rakstit komentārus
+print("Sveiki, "+"sveiki, "+"sveiki, "+"sveiki, "+"sveiki")
+print("Daniels "*7)
+print(60*60*24*365)
+print(0.1+0.2-0.3)
